8e7e73b022e42e872f4eec1ecf24c02fe38dad3f
sasham43 <sashasemail@gmail.com>
2016-04-04 10:12:02 -0500

    Change generated button ids to classes since there are multiples.  Would have figured having ids would have broken functionality but it worked

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ $(function(){
     // increment click counter
     generateClicked++;
     // create new string with div, two buttons, and span with id generated from number of times generate button was clicked
-    var buttonContainer = '<div class=\"container\"><button id=\"delete\">Delete</button><button id=\"change\">Change</button><span id=\"generateClicked' + generateClicked + '\"><span></div>';
+    var buttonContainer = '<div class=\"container\"><button class=\"delete\">Delete</button><button class=\"change\">Change</button><span id=\"generateClicked' + generateClicked + '\"><span></div>';
     // append above container onto DOM body
     $("body").append(buttonContainer);
     // target the most recently created span
@@ -14,12 +14,12 @@ $(function(){
     $(spanName).append(generateClicked);
   })
 
-  $(document).on('click', "#delete",function(){
+  $(document).on('click', ".delete", function(){
     // remove div container when clicked
     $(this).parent().remove();
   });
 
-  $(document).on('click', "#change", function(){
+  $(document).on('click', ".change", function(){
     // change background color to red when clicked
     $(this).parent().toggleClass("red");
   });
